c0ee597a6e1c6e89f3db487e6fc67b09093c3911
Aniruddha1701 <aniruddha1701@gmail.com>
2025-12-19 14:49:33 +0530

    fix api serverless

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
 // Vercel Serverless Function Entry Point
+// This wraps the Express app for Vercel's serverless environment
+
 const app = require('../server/index.js');
 
-module.exports = app;
+// Export the Express app as a serverless function
+module.exports = (req, res) => {
+    // Handle the request with Express
+    return app(req, res);
+};
